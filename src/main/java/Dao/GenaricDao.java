7c05c96a47910d0c7e65f4d53a3ad8fd7057f44b
daicatimmai123v30 <49441221+daicatimmai123v30@users.noreply.github.com>
2021-10-01 22:46:37 +0700

    Delete src/main directory

--- src/main/java/Dao/GenaricDao.java
+++
@@ -1,9 +0,0 @@
-package Dao;
-
-import java.util.List;
-
-import Mapper.RowMapper;
-
-public interface GenaricDao<T> {
-	T query(String Sql,RowMapper<T> rowMapper, Object... parameters) ;
-}
--- src/main/java/Mapper/RowMapper.java
+++
@@ -1,7 +0,0 @@
-package Mapper;
-
-import java.sql.ResultSet;
-
-public interface RowMapper<T>{
-	T MapRow(ResultSet resultset);
-}
